bedc3e08dbb6152b14cf529c6de3b6a46893c0be
Fan Wang <wangfanbsg75@live.com>
2020-03-13 19:36:13 -0500

    cumstat much faster

--- R/ff_panel_cumsum.R
+++ R/ff_panel_cumsum.R
@@ -1,4 +1,4 @@
-ff_panel_cumsum_grouplast <- function(df, svr_id, svr_x, svr_y, svr_cumsumtop = 'y_movingavg_lastestscores', stat='mean') {
+ff_panel_cumsum_grouplast <- function(df, svr_id, svr_x, svr_y, svr_cumsumtop = 'y_movingavg_lastestscores', stat='mean', quick=TRUE, verbose=FALSE) {
     #' Cumulative stat value from each group from the latest date of availability. Sum or Average
     #'
     #' @description
@@ -11,6 +11,9 @@ ff_panel_cumsum_grouplast <- function(df, svr_id, svr_x, svr_y, svr_cumsumtop = 
     #' @param svr_x string name of the ranking variable
     #' @param svr_y string name of the value variable
     #' @param svr_cumsumtop string variable name that stores the cumulative summed variable
+    #' @param stat string type of statistics to compute, mean, sum, max, min, median
+    #' @param quick boolean faster algorithm without repeating calculation, quick should be used, slow was initial bad algorithm, results identical
+    #' @param verbose boolean when quick is FALSE, could verbose which generates slower results
     #' @return a dataframe with the cumulative summed/averaged etc column up to each row
     #' @author Fan Wang, \url{http://fanwangecon.github.io}
     #' @references
@@ -18,11 +21,12 @@ ff_panel_cumsum_grouplast <- function(df, svr_id, svr_x, svr_y, svr_cumsumtop = 
     #' \url{https://fanwangecon.github.io/REconTools/articles/fv_panel_cumsum_grouplast.html}
     #' \url{https://github.com/FanWangEcon/REconTools/blob/master/R/ff_panel_cumsum.R}
     #' @export
-    #' @import dplyr tidyr tibble
+    #' @import dplyr tidyr tibble rlang
     #' @examples
     #' library(tidyr)
     #' library(dplyr)
     #' library(tibble)
+    #' set.seed(12345)
     #' it_N <- 5
     #' ar_it_M <- sample(1:10, it_N, replace = TRUE)
     #' ar_it_M_ID <- sample(1:it_N, it_N, replace = FALSE)
@@ -32,31 +36,98 @@ ff_panel_cumsum_grouplast <- function(df, svr_id, svr_x, svr_y, svr_cumsumtop = 
     #' tb_long <- tb_long %>% arrange(id, x) %>% group_by(id) %>% mutate(rank_l = row_number())
     #' df <- tb_long %>% select(id, x) %>% add_column(y = runif(dim(tb_long)[1])) %>% arrange(id,x) %>% group_by(id) %>% mutate(y = cumsum(y))
     #' print(df)
-    #' ff_panel_cumsum_grouplast(df, svr_id='id', svr_x='x', svr_y='y', svr_cumsumtop = 'y_movingavg_lastestscore', stat='mean')
+    #' quick <- TRUE
+    #' ff_panel_cumsum_grouplast(df, svr_id='id', svr_x='x', svr_y='y', svr_cumsumtop = 'y_movingavg_lastestscore', stat='mean', quick=quick)
+    #' quick <- FALSE
+    #' verbose <- TRUE
+    #' ff_panel_cumsum_grouplast(df, svr_id='id', svr_x='x', svr_y='y', svr_cumsumtop = 'y_movingavg_lastestscore', stat='mean', quick=quick, verbose=verbose)
+    #' verbose <- FALSE
+    #' ff_panel_cumsum_grouplast(df, svr_id='id', svr_x='x', svr_y='y', svr_cumsumtop = 'y_movingavg_lastestscore', stat='mean', quick=quick, verbose=verbose)
+    #' ff_panel_cumsum_grouplast(df, svr_id='id', svr_x='x', svr_y='y', svr_cumsumtop = 'y_movingsum_lastestscore', stat='median')
+    #' ff_panel_cumsum_grouplast(df, svr_id='id', svr_x='x', svr_y='y', svr_cumsumtop = 'y_movingsum_lastestscore', stat='sum')
+    #' ff_panel_cumsum_grouplast(df, svr_id='id', svr_x='x', svr_y='y', svr_cumsumtop = 'y_movingsum_lastestscore', stat='min')
+    #' ff_panel_cumsum_grouplast(df, svr_id='id', svr_x='x', svr_y='y', svr_cumsumtop = 'y_movingsum_lastestscore', stat='max')
 
     tb_data <- df %>% arrange(x)
     tb_data_cum_sum_top <- tb_data %>% mutate(!!sym(svr_cumsumtop) := 0)
+    ar_latest_indi <- rep(NA, 1, length(unique(tb_data[['id']])))
 
-    for (row_ctr in seq(1, dim(tb_data)[1])) {
+    # This is the quicker algorithm that saves a lot of time hopefully
+    if (quick) {
 
-        # select up to current row sort and group
-        tb_data_up2row <- tb_data[1:row_ctr,] %>% arrange(id, x) %>% group_by(id)
-
-        # Obtain last element sorted by x for each group, and resort by x
-        tb_data_up2row <- tb_data_up2row %>% slice(n()) %>% arrange(x) %>% ungroup()
-
-        # cumulative sum of the highest element of each group below row_ctr
         if (stat == 'sum'){
-          fl_cum_sum_top_cur <- tb_data_up2row %>% summarize(y_sum_top_up2row = sum(y)) %>% pull(y_sum_top_up2row)
+          for (row_ctr in seq(1, dim(tb_data)[1])) {
+              ar_latest_indi[tb_data[['id']][row_ctr]] <- tb_data[['y']][row_ctr]
+              fl_cum_sum_top_cur <- sum(ar_latest_indi, na.rm = TRUE)
+          }
+          tb_data_cum_sum_top[row_ctr, svr_cumsumtop] <- fl_cum_sum_top_cur
         } else if (stat == 'mean'){
-          fl_cum_sum_top_cur <- tb_data_up2row %>% summarize(y_sum_top_up2row = mean(y)) %>% pull(y_sum_top_up2row)
-        } else {
-          warning('function stat not available')
+          for (row_ctr in seq(1, dim(tb_data)[1])) {
+              ar_latest_indi[tb_data[['id']][row_ctr]] <- tb_data[['y']][row_ctr]
+              fl_cum_sum_top_cur <- mean(ar_latest_indi, na.rm = TRUE)
+          }
+          tb_data_cum_sum_top[row_ctr, svr_cumsumtop] <- fl_cum_sum_top_cur
+        } else if (stat == 'median'){
+          for (row_ctr in seq(1, dim(tb_data)[1])) {
+              ar_latest_indi[tb_data[['id']][row_ctr]] <- tb_data[['y']][row_ctr]
+              fl_cum_sum_top_cur <- median(ar_latest_indi, na.rm = TRUE)
+          }
+          tb_data_cum_sum_top[row_ctr, svr_cumsumtop] <- fl_cum_sum_top_cur
+        } else if (stat == 'max'){
+          for (row_ctr in seq(1, dim(tb_data)[1])) {
+              ar_latest_indi[tb_data[['id']][row_ctr]] <- tb_data[['y']][row_ctr]
+              fl_cum_sum_top_cur <- max(ar_latest_indi, na.rm = TRUE)
+          }
+          tb_data_cum_sum_top[row_ctr, svr_cumsumtop] <- fl_cum_sum_top_cur
+        } else if (stat == 'min'){
+          for (row_ctr in seq(1, dim(tb_data)[1])) {
+              ar_latest_indi[tb_data[['id']][row_ctr]] <- tb_data[['y']][row_ctr]
+              fl_cum_sum_top_cur <- min(ar_latest_indi, na.rm = TRUE)
+          }
+          tb_data_cum_sum_top[row_ctr, svr_cumsumtop] <- fl_cum_sum_top_cur
         }
 
-        # Store results
-        tb_data_cum_sum_top[row_ctr, svr_cumsumtop] <- fl_cum_sum_top_cur
+    } else {
+
+        for (row_ctr in seq(1, dim(tb_data)[1])) {
+          # The operations below have a lot of repetition, at each x, only 1 piece of information is added, but redoing all calculations
+
+          if (verbose) {
+            # select up to current row sort and group
+            tb_data_up2row <- tb_data[1:row_ctr,] %>% arrange(id, x) %>% group_by(id)
+
+            # Obtain last element sorted by x for each group, and resort by x
+            tb_data_up2row <- tb_data_up2row %>% slice(n()) %>% arrange(x) %>% ungroup()
+
+            # cumulative sum of the highest element of each group below row_ctr
+            # Statistics, sum, mean, etc are not within individual, but across individual
+            # within individual, always simply taking the last individual with slice(n())
+            if (stat == 'sum'){
+              fl_cum_sum_top_cur <- tb_data_up2row %>% summarize(y_sum_top_up2row = sum(y)) %>% pull(y_sum_top_up2row)
+            } else if (stat == 'mean'){
+              fl_cum_sum_top_cur <- tb_data_up2row %>% summarize(y_sum_top_up2row = mean(y)) %>% pull(y_sum_top_up2row)
+            } else if (stat == 'median'){
+              fl_cum_sum_top_cur <- tb_data_up2row %>% summarize(y_sum_top_up2row = median(y)) %>% pull(y_sum_top_up2row)
+            } else if (stat == 'max'){
+              fl_cum_sum_top_cur <- tb_data_up2row %>% summarize(y_sum_top_up2row = max(y)) %>% pull(y_sum_top_up2row)
+            } else if (stat == 'min'){
+              fl_cum_sum_top_cur <- tb_data_up2row %>% summarize(y_sum_top_up2row = min(y)) %>% pull(y_sum_top_up2row)
+            } else {
+              warning('function stat not available')
+            }
+          } else {
+            func = paste0(stat, '(y)')
+            fl_cum_sum_top_cur <- tb_data[1:row_ctr,] %>% arrange(id, x) %>%
+                group_by(id) %>% slice(n()) %>% ungroup() %>%
+                summarize(y_sum_top_up2row = !!parse_expr(func)) %>% pull()
+          }
+
+          # Store results
+          tb_data_cum_sum_top[row_ctr, svr_cumsumtop] <- fl_cum_sum_top_cur
+      }
+
     }
+
 
     # return
     return(tb_data_cum_sum_top)
